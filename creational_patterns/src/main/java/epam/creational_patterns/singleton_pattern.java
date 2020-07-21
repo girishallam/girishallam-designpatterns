cb526e16fdf73353af9f8862f98bee1cb313f380
girishallam <60657607+girishallam@users.noreply.github.com>
2020-07-21 22:34:49 +0530

    Add files via upload

---
+++ creational_patterns/src/main/java/epam/creational_patterns/singleton_pattern.java
@@ -0,0 +1,25 @@
+package epam.creational_patterns;
+class SingleObject {
+
+	   private static SingleObject instance = new SingleObject();
+
+	   private SingleObject(){}
+
+	   public static SingleObject getInstance(){
+	      return instance;
+	   }
+
+	   public void showMessage(){
+	      System.out.println("Hello World!");
+	   }
+	}
+public class singleton_pattern {
+
+	public static void main(String[] args) {
+		SingleObject object = SingleObject.getInstance();
+
+	      object.showMessage();
+
+	}
+
+}
